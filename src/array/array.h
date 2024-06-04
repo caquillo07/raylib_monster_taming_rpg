1f6f050e763394fd121b494045d9b3a54164fa38
Hector Mejia <caquillo722@gmail.com>
2024-06-03 23:30:44 -0400

    array_range macro, nice

--- src/array/array.h
+++ src/array/array.h
@@ -7,6 +7,9 @@
         (array)[array_length(array) - 1] = (value);                           \
     } while (0);
 
+#define array_range(array, index) \
+    for(i32 (index) = 0; (index) < array_length(array); (index)++)
+
 void* array_hold(void* array, int count, int item_size);
 void array_remove(void* array, int index, int item_size);
 int array_length(void* array);
